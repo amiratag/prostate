0bd75bd3372cdaa90990b82a6c4e5b4708c05214
amiratag <amiratag@stanford.edu>
2021-01-28 17:15:54 -0800

    Update load_model.py

--- load_model.py
+++ load_model.py
@@ -6,6 +6,6 @@ num_classes = 6
 model = DenseNet(num_classes)
 if use_cuda:
     model = torch.nn.DataParallel(model).cuda()
-checkpoint = torch.load('checkpoint-epoch60.pt')
+checkpoint = torch.load('checkpoint-epoch100.pt')
 state_dict = checkpoint.get('state_dict', checkpoint)
 model.load_state_dict(state_dict)
